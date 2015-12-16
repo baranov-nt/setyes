e98289261ab8d297dcd1977b669e4b12f28ce346
phpnt <admin@phpnt.com>
2015-12-16 07:08:46 +0500

    Google Autocomplite a6

--- backend/controllers/MapsController.php
+++ backend/controllers/MapsController.php
@@ -8,7 +8,6 @@
 namespace backend\controllers;
 
 use Yii;
-use yii\web\Cookie;
 
 class MapsController extends BehaviorsController
 {
@@ -24,42 +23,47 @@ class MapsController extends BehaviorsController
     public function actionSelectCity()
     {
         $place = Yii::$app->request->post('place');
-        //d($place);
         $object = \Yii::$app->googleApi->getGeoCodeObject($place, null);
 
-        $city = '';
-        foreach($object->address_components as $one):
-            if($one->types[0] == 'administrative_area_level_2'):
-                $city .= $one->short_name.' ';
+        if(isset($object)):
+            $city = '';
+            foreach($object->address_components as $one):
+                if($one->types[0] == 'locality'):
+                    $city .= $one->short_name.' ';
+                endif;
+                /*if($one->types[0] == 'administrative_area_level_2'):
+                    $city .= $one->short_name.' ';
+                endif;*/
+                if($one->types[0] == 'administrative_area_level_1'):
+                    $city .= $one->short_name.' ';
+                endif;
+                if($one->types[0] == 'country'):
+                    $city .= $one->short_name.' ';
+                endif;
+            endforeach;
+            $object = \Yii::$app->googleApi->getGeoCodeObject($city, null);
+            $formattedAddress = $object->formatted_address;
+            $idPlace = $object->place_id;
+
+            if($formattedAddress != null && $idPlace != null):
+                $cookies = Yii::$app->response->cookies;
+
+                $cookies->add(new \yii\web\Cookie([
+                    'name' => '_city',
+                    'value' => $formattedAddress,
+                    'expire' => time() + 86400 * 365,
+                ]));
+
+                $cookies->add(new \yii\web\Cookie([
+                    'name' => '_placeId',
+                    'value' => $idPlace,
+                    'expire' => time() + 86400 * 365,
+                ]));
+            else:
+                $cookies = Yii::$app->response->cookies;
+                $cookies->remove('_city');
+                $cookies->remove('_placeId');
             endif;
-            if($one->types[0] == 'administrative_area_level_1'):
-                $city .= $one->short_name.' ';
-            endif;
-            if($one->types[0] == 'country'):
-                $city .= $one->short_name.' ';
-            endif;
-        endforeach;
-        //d($city);
-        $object = \Yii::$app->googleApi->getGeoCodeObject($city, null);
-        $formattedAddress = $object->formatted_address;
-        $idPlace = $object->place_id;
-
-        //dd([$formattedAddress, $idPlace]);
-
-        if($formattedAddress != null && $idPlace != null):
-            $cookies = Yii::$app->response->cookies;
-
-            $cookies->add(new \yii\web\Cookie([
-                'name' => '_city',
-                'value' => $formattedAddress,
-                'expire' => time() + 86400 * 365,
-            ]));
-
-            $cookies->add(new \yii\web\Cookie([
-                'name' => '_placeId',
-                'value' => $idPlace,
-                'expire' => time() + 86400 * 365,
-            ]));
         else:
             $cookies = Yii::$app->response->cookies;
             $cookies->remove('_city');
--- frontend/controllers/MainController.php
+++ frontend/controllers/MainController.php
@@ -15,7 +15,6 @@ use yii\helpers\Url;
 use frontend\models\AccountActivation;
 use common\models\Carousel;
 use yii\web\ErrorAction;
-use yii\web\Cookie;
 
 class MainController extends BehaviorsController
 {
@@ -323,42 +322,47 @@ class MainController extends BehaviorsController
     public function actionSelectCity()
     {
         $place = Yii::$app->request->post('place');
-        //d($place);
         $object = \Yii::$app->googleApi->getGeoCodeObject($place, null);
 
-        $city = '';
-        foreach($object->address_components as $one):
-            if($one->types[0] == 'administrative_area_level_2'):
-                $city .= $one->short_name.' ';
+        if(isset($object)):
+            $city = '';
+            foreach($object->address_components as $one):
+                if($one->types[0] == 'locality'):
+                    $city .= $one->short_name.' ';
+                endif;
+                /*if($one->types[0] == 'administrative_area_level_2'):
+                    $city .= $one->short_name.' ';
+                endif;*/
+                if($one->types[0] == 'administrative_area_level_1'):
+                    $city .= $one->short_name.' ';
+                endif;
+                if($one->types[0] == 'country'):
+                    $city .= $one->short_name.' ';
+                endif;
+            endforeach;
+            $object = \Yii::$app->googleApi->getGeoCodeObject($city, null);
+            $formattedAddress = $object->formatted_address;
+            $idPlace = $object->place_id;
+
+            if($formattedAddress != null && $idPlace != null):
+                $cookies = Yii::$app->response->cookies;
+
+                $cookies->add(new \yii\web\Cookie([
+                    'name' => '_city',
+                    'value' => $formattedAddress,
+                    'expire' => time() + 86400 * 365,
+                ]));
+
+                $cookies->add(new \yii\web\Cookie([
+                    'name' => '_placeId',
+                    'value' => $idPlace,
+                    'expire' => time() + 86400 * 365,
+                ]));
+            else:
+                $cookies = Yii::$app->response->cookies;
+                $cookies->remove('_city');
+                $cookies->remove('_placeId');
             endif;
-            if($one->types[0] == 'administrative_area_level_1'):
-                $city .= $one->short_name.' ';
-            endif;
-            if($one->types[0] == 'country'):
-                $city .= $one->short_name.' ';
-            endif;
-        endforeach;
-        //d($city);
-        $object = \Yii::$app->googleApi->getGeoCodeObject($city, null);
-        $formattedAddress = $object->formatted_address;
-        $idPlace = $object->place_id;
-
-        //dd([$formattedAddress, $idPlace]);
-
-        if($formattedAddress != null && $idPlace != null):
-            $cookies = Yii::$app->response->cookies;
-
-            $cookies->add(new \yii\web\Cookie([
-                'name' => '_city',
-                'value' => $formattedAddress,
-                'expire' => time() + 86400 * 365,
-            ]));
-
-            $cookies->add(new \yii\web\Cookie([
-                'name' => '_placeId',
-                'value' => $idPlace,
-                'expire' => time() + 86400 * 365,
-            ]));
         else:
             $cookies = Yii::$app->response->cookies;
             $cookies->remove('_city');

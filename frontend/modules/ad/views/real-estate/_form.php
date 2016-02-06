56bfb92a3aaa75fddcda59a01bb49ee0abb20495
Vladimir Baranov <baranov.vs@yandex.ru>
2016-02-06 17:21:17 +0500

    add realEstate a16

--- frontend/modules/ad/views/real-estate/_form.php
+++ frontend/modules/ad/views/real-estate/_form.php
@@ -7,6 +7,7 @@ use yii\widgets\MaskedInput;
 use common\widgets\GooglePlacesAutoComplete\GooglePlacesAutoComplete;
 use common\widgets\ImageLoad\ImageLoadWidget;
 use common\widgets\ImageLoad\assets\CropperAsset;
+use yii\widgets\Pjax;
 
 CropperAsset::register($this);
 
@@ -18,12 +19,77 @@ CropperAsset::register($this);
 /* @var $pjaxUrl string */
 
 $user = Yii::$app->user->identity;
-d($modelAdRealEstate->adCategories->imagesOfObjects);
 ?>
 
-
+<div class="row">
+    <div class="col-md-12">
+        <?php
+        $user = Yii::$app->user->identity;
+        //dd($user->userPrivilege->images_num);
+        echo \common\widgets\ImageLoad\ImageLoadWidget::widget([
+            'modelName' => 'AdRealEstate',
+            'id' => 'load-image',                                       // суффикс ID для основных форм виджета
+            'object_id' => $modelAdRealEstate->id,                          // ID объекта, к которому привязаны изображения
+            'imagesObject' => $modelAdRealEstate->imagesOfObjects,          // объект с загруженными для модели изображениями
+            'images_num' => $user->userPrivilege->images_num,                 // максимальное количество изображений
+            'images_label' => $modelAdRealEstate->images_label,             // максимальное количество изображений
+            'images_temp' => 0,       // указываем временной изображение или нет
+            'imageSmallWidth' => 360,                       // ширина миниатюры
+            'imageSmallHeight' => 200,                      // высота миниатюры
+            'headerModal' => 'Загрузить изображение товара',                        // заголовок в модальном окне
+            'sizeModal' => 'modal-md',                                  // размер модального окна
+            'baseUrl' => '/images/',                        // основной путь к изображениям
+            'imagePath' => 'imagesApp/images/',   // путь, куда будут записыватся изображения
+            'noImage' => 'imagesApp/noImage.png',                 // картинка, если изображение отсутствует
+            'classesWidget' => [
+                'imageClass' => 'imageProduct',
+                'buttonDeleteClass' => 'btn btn-xs btn-danger btn-imageDeleteProduct glyphicon glyphicon-trash glyphicon',
+                'imageContainerClass' => 'imageContainerProduct',
+                'formImagesContainerClass' => 'formImageContainerProduct',
+            ],
+            'pluginOptions' => [                            // настройки плагина
+                'aspectRatio' => 16/9,                      // установите соотношение сторон рамки обрезки. По умолчанию свободное отношение.
+                'strict' => true,                           // true - рамка не может вызодить за холст, false - может
+                'guides' => true,                           // показывать пунктирные линии в рамке
+                'center' => true,                           // показывать центр в рамке изображения изображения
+                'autoCrop' => true,                         // показывать рамку обрезки при загрузке
+                'autoCropArea' => 0.5,                      // площидь рамки на холсте изображения при autoCrop (1 = 100% - 0 - 0%)
+                'dragCrop' => true,                         // создание новой рамки при клики в свободное место хоста (false - нельзя)
+                'movable' => true,                          // перемещать изображение холста (false - нельзя)
+                'rotatable' => true,                        // позволяет вращать изображение
+                'scalable' => true,                         // мастабирование изображения
+                'zoomable' => true,
+                'preview' => '.img-preview',                // класс превью
+            ],
+            'cropBoxData' => [                              // начальные настройки рамки // cropBoxData = { left: 10, top: 10, width: 160, height:200 }
+                'left' => 10,                               // смещение слева
+                'top' => 10,                                // смещение вниз
+                //'width' => 160,                             // ширина
+                //'height' => 200                             // высота
+            ],
+            'canvasData' => [                               // начальные настройки холста
+                //'width' => 500,                             // ширина
+                //'height' => 500                             // высота
+            ]]);
+        ?>
+    </div>
+</div>
 <div class="ad-real-estate-form">
-    <?php $form = ActiveForm::begin([
+    <?php
+    Pjax::begin([
+        'enablePushState' => false,
+    ]);
+    ?>
+
+    <?php
+    MaskedInput::widget([
+        'name' => 'masked-input_init',
+        'clientOptions' => [
+            'alias' => 'decimal',
+        ],
+    ]);
+
+    $form = ActiveForm::begin([
         'fieldClass' => ActiveField::className(),
         'id' => 'ad_form',
     ]); ?>
@@ -398,5 +464,7 @@ d($modelAdRealEstate->adCategories->imagesOfObjects);
     </div>
 
     <?php ActiveForm::end(); ?>
-
+    <?php
+    Pjax::end();
+    ?>
 </div>

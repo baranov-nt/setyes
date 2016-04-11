3be638fa2421491d8a64553594152a5813c86f39
Vladimir Baranov <baranov.vs@yandex.ru>
2016-04-11 19:04:52 +0500

    realEstate moving 23

--- frontend/web/js/notification.js
+++ frontend/web/js/notification.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
     var socket = io.connect('http://localhost:8890');
     socket.on('notification', function (data) {
         var message = JSON.parse(data);
+        $("#chatBlock").show();
         $( "#notifications" ).prepend( "<p style='font-size: 11px;'><strong>" + message.name + "</strong>: " + message.message + "<br><strong><i style='font-size: 8px;'>(" + message.time + ")</i></strong></p>");
     });
 });
